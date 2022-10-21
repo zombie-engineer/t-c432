87d200d79d3702bf56dc5d3ef64711ef36c76813
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2022-10-22 02:40:27 +0300

    ui Moved to separate files

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include "i2c.h"
 #include "gpio.h"
 #include "ssd1306.h"
+#include "ui.h"
 #include "scb.h"
-#include "font.h"
 #include "tim.h"
 #include "usart.h"
 #include "nvic.h"
@@ -34,124 +34,6 @@ int x = 0;
 int dig = 0;
 char buf[32];
 int tick = 0;
-
-struct bar_widget {
-  int pos_x;
-  int pos_y;
-  int size_x;
-  int size_y;
-  int level;
-};
-
-void bar_widget_draw(const struct bar_widget *b)
-{
-  dbuf_draw_rect(
-    b->pos_x, b->pos_y,
-    b->pos_x + b->size_x,
-    b->pos_y + b->size_y, 1);
-
-  dbuf_draw_hatched_rect(
-    b->pos_x + 2,
-    b->pos_y,
-    b->pos_x + b->size_x - 1,
-    b->pos_y + b->level,
-    1);
-
-  dbuf_draw_line(
-    b->pos_x + 2,
-    b->pos_y + b->level,
-    b->pos_x + b->size_x - 1,
-    b->pos_y + b->level,
-    1);
-}
-
-void draw_tim2_cntr(int x, int y)
-{
-  char counterbuf[32];
-  itoa(tim2_read_counter_value(), counterbuf, 10);
-  x = dbuf_draw_text(x, y, "TIM2_TCNT:", &font_1);
-  dbuf_draw_text(x, y, counterbuf, &font_2);
-}
-
-void draw_dynamic_bar(int level)
-{
-  struct bar_widget w = {
-    .pos_x = 110,
-    .pos_y = 0,
-    .size_x = 10,
-    .size_y = 63,
-    .level = level
-  };
-  bar_widget_draw(&w);
-}
-
-void draw_voltmeter(int x, int y, int volt_int, int volt_frac)
-{
-  char b[32];
-  char *p = b;
-  itoa(volt_int, p, 10);
-  p += strlen(p);
-  *p++ = '.';
-  itoa(volt_frac, p, 10);
-  p += strlen(p);
-  x = dbuf_draw_text(x, y, "V:", &font_3);
-  dbuf_draw_text(x, y, b, &font_3);
-}
-
-int frame_counter = 0;
-int duration;
-
-void draw_blinker_icon(int x, int y, int sz, int num_frames, int interval)
-{
-  if (!duration && !(frame_counter % interval)) {
-    duration = num_frames;
-  }
-  if (duration) {
-    dbuf_draw_filled_rect(x, y, x + sz, y + sz, 1);
-    duration--;
-  }
-}
-
-#if 0
-static void update_display_usb_stats()
-{
-  char buf[32];
-  dbuf_draw_text(20, 4, "r:", &font_1);
-  itoa(usbstats.num_resets, buf, 10);
-  dbuf_draw_text(28, 4, buf, &font_1);
-  itoa(usbstats.num_errs, buf, 10);
-  dbuf_draw_text(34, 4, buf, &font_1);
-  itoa(usbstats.num_transactions, buf, 10);
-  dbuf_draw_text(40, 4, buf, &font_1);
-}
-#endif
-
-void update_display()
-{
-  frame_counter++;
-  int y;
-  float adc_normalized = 0.0f;// (float)last_adc / 4096;
-  int level = adc_normalized * 64;
-  int num_volt = adc_normalized * 3300;
-  int first = num_volt / 1000;
-  int next = num_volt - first * 1000;
-
-
-  dbuf_clear();
-  // update_display_usb_stats();
-  draw_dynamic_bar(level);
-  draw_voltmeter(2, 35, first, next);
-
-  y = 40;
-  dbuf_draw_line(64, y, 64, y + 3, 1);
-  y = 20;
-  dbuf_draw_line(64, y, 64, y + 3, 1);
-  y = 5;
-  dbuf_draw_line(64, y, 64, y + 3, 1);
-  draw_tim2_cntr(10, 50);
-  draw_blinker_icon(89, 6, 5, 3, 13);
-  dbuf_flush();
-}
 
 void timer_setup(void)
 {
@@ -208,7 +90,7 @@ void main(void)
   ssd1306_init();
   timer_setup();
   debug_pin_setup();
-  update_display();
+  ui_update();
   usb_init();
 //  uart2_setup();
 
