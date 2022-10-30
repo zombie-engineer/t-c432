f597bd46578a881d4bb21dc362cd06ba152a59dd
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2022-10-30 13:27:41 +0300

    common: added container_of macro

--- common_util.h
+++ common_util.h
@@ -1,6 +1,13 @@
 #pragma once
+#include <stddef.h>
 
 #define min(__a, __b) ((__a) < (__b) ? (__a) : (__b))
 
 #define ARRAY_SIZE(__a) (sizeof(__a) / sizeof(__a[0]))
 
+/*
+ * linux-kernel style macro, arguments are the same on purpose to not
+ * confuse developers offsetof is supported by gcc
+ */
+#define container_of(__ptr, __type, __member) \
+  (__type *)(((char *)(__ptr)) - offsetof(__type, __member))
