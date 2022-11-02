8e52a264ea2a13c6f6ec7567afe200ff9cc8642c
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2022-11-02 10:57:02 +0300

    nvic: Added interrupt enable API, complete interrupts list

--- nvic.c
+++ nvic.c
@@ -1,5 +1,6 @@
 #include "nvic.h"
 #include "reg_access.h"
+#include "svc.h"
 
 uint8_t nvic_get_priority(int interrupt_no)
 {
@@ -11,4 +12,16 @@ void nvic_set_priority(int interrupt_no, uint8_t pri)
 {
   volatile uint8_t *reg = ((volatile uint8_t *)NVIC_IPR0) + interrupt_no;
   *reg = (pri & 0xf) << 4;
+}
+
+void nvic_enable_interrupt(int interrupt_no)
+{
+  volatile uint32_t *r;
+
+  if (interrupt_no > NVIC_INTERRUPT_NUMBER_MAX) {
+    svc_call(SVC_PANIC);
+  }
+
+  r = NVIC_ISER0 + interrupt_no / NVIC_ICER_INTS_PER_REG;
+  reg32_set_bit(r, interrupt_no % NVIC_ICER_INTS_PER_REG);
 }
