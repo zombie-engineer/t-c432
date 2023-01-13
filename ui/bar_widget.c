900cfe0b167a99672219947d500f087c9235e0ff
Valeriy Vdovin <valery.vdovin.s@gmail.com>
2023-01-14 01:40:51 +0300

    fixed bar widget field name

--- ui/bar_widget.c
+++ ui/bar_widget.c
@@ -8,7 +8,7 @@ struct bar_widget_priv {
 void bar_widget_draw(struct widget *w)
 {
   struct bar_widget_priv *p;
-  p = w->data;
+  p = w->priv;
   dbuf_draw_rect(
     w->pos_x, w->pos_y,
     w->pos_x + w->size_x,
@@ -38,7 +38,7 @@ void bar_widget_handle_event(struct widget *w, ui_event_type ev, int param)
 void bar_widget_init(struct widget *w)
 {
   bar_widget_priv.level = 0;
-  w->data = &bar_widget_priv;
+  w->priv = &bar_widget_priv;
   w->draw = bar_widget_draw;
   w->handle_event = bar_widget_handle_event;
   w->pos_x = 0;
